13a06606fb532d3ca41fc1fb949e7541beba3c55
Eric Ferguson <ferguson.eric.r@gmail.com>
2019-12-06 17:10:21 -0800

    added -- class App extends Component, set up render()

--- github-user-card-react/src/App.js
+++ github-user-card-react/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+
+  render(){
+    return (
+      <>
+      </>
+    )
+  }
 }
 
 export default App;
