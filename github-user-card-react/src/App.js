4356cf19281f4bd0c98fd38e781fdfe4f91fa08c
Eric Ferguson <ferguson.eric.r@gmail.com>
2019-12-06 18:27:32 -0800

    set up axios call, stuck here

--- github-user-card-react/src/App.js
+++ github-user-card-react/src/App.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+    }
+  }
+  componentDidMount(){
+    this.fetchUserDetails()
+  }
+
+  fetchUserDetails = () => {
+    axios
+      .get(`https://api.github.com/users/erferguson`)
+      .then(result => {
+        console.log(result);
+        this.setState({
+        })
+
+      })
+      .catch(error => {
+        console.log('Error, oh to error:', error)
+      })
+  }
 
   render(){
     return (
       <>
+      <h1>Github User Card</h1>
       </>
     )
   }
