e03859c142cc27ebbf4fa847fdc5ccc7abcac692
Eric Ferguson <ferguson.eric.r@gmail.com>
2019-12-10 11:09:30 -0800

    added styling to cardcomponent & following, basic

--- github-user-card-react/src/components/Following.js
+++ github-user-card-react/src/components/Following.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 
 import axios from 'axios';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
 class Following extends Component {
     constructor(){
         super();
@@ -31,10 +40,18 @@ class Following extends Component {
                 {following.map(followed => {
                     {console.log(followed)}
                     return (
-                    <>
-                        <img src={followed.avatar_url} />
-                        <p>{followed.login}</p>
-                    </>
+                    <div className='following-div'>
+                        <Card className='following-card'>
+                            <CardActionArea>
+                                <CardContent className='follower-content'>
+                                    <Typography>
+                                        <img src={followed.avatar_url} />
+                                        <p>{followed.login}</p>
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </div>
                     )
                 })}
             </>
