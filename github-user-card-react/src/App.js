7cd192f5d003b46828047bc06c4b003548e64385
Eric Ferguson <ferguson.eric.r@gmail.com>
2019-12-09 17:18:17 -0800

    updated App.js, removed constructor,super, this.state, props

--- github-user-card-react/src/App.js
+++ github-user-card-react/src/App.js
@@ -2,48 +2,47 @@ import React, { Component } from 'react';
 
 import CardComponent from './components/CardComponent';
 import Followers from './components/Followers';
-import axios from 'axios';
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      user: {
-        id: '',
-        name: 'Eric',
-        login: 'erferguson',
-        avatar_url: '',
-        html_url: '',
-        bio: '',
-        location: '',
-        followers: 0,
-        followers_url: '',
-        following: 7,
-        following_url: ''
-      },
-      followers: [{
-        id: '',
-        name: 'Eric',
-        login: 'erferguson',
-        avatar_url: '',
-        html_url: '',
-        bio: '',
-        location: '',
-        followers: 0,
-        followers_url: '',
-        following: 7,
-        following_url: ''
-      }]
-    }
-  }
+  // constructor(){
+  //   super();
+  //   this.state = {
+  //     user: {
+  //       id: '',
+  //       name: 'Eric',
+  //       login: 'erferguson',
+  //       avatar_url: '',
+  //       html_url: '',
+  //       bio: '',
+  //       location: '',
+  //       followers: 0,
+  //       followers_url: '',
+  //       following: 7,
+  //       following_url: ''
+  //     },
+  //     followers: [{
+  //       id: '',
+  //       name: 'Eric',
+  //       login: 'erferguson',
+  //       avatar_url: '',
+  //       html_url: '',
+  //       bio: '',
+  //       location: '',
+  //       followers: 0,
+  //       followers_url: '',
+  //       following: 7,
+  //       following_url: ''
+  //     }]
+  //   }
+  // }
   
 
   render(){
     return (
       <>
       <h1>Github User Card</h1>
-      <CardComponent user={this.state.user}/>
-      <Followers followers={this.state.followers}/>
+      <CardComponent />
+      <Followers />
       </>
     )
   }
